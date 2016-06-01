1978d25ddaff631324675073cdf42bc625fd3868
Cleve Gomes <Cleve Gomes>
2016-06-01 08:45:00 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# angular2-course
+````
+Contains The following
+1.Charts
+2.Pagination
+3.Validation
+4.Forms
+5.Loaders
+````
---
+++ app/chart.component.ts
@@ -0,0 +1,68 @@
+import {Component} from "@angular/core";
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from '@angular/common';
+
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
+
+
+
+//
+// import 'chartjs';
+// declare let Chart;
+
+
+
+
+@Component({
+    selector: "line-chart-demo",
+    templateUrl: 'app/chart.component.html',
+    styles: ['.chart {display: block; width: 100%;}'],
+    directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES]
+})
+
+export class ChartComponent {
+
+    // lineChart
+    public lineChartData:Array<any> = [
+        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
+    ];
+    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartOptions:any = {
+        animation: false,
+        responsive: true
+    };
+    public lineChartColours:Array<any> = [
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        }
+
+    ];
+    public lineChartLegend:boolean = true;
+    public lineChartType:string = 'line';
+
+    public randomize():void {
+        console.log("came");
+        let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+        for (let i = 0; i < this.lineChartData.length; i++) {
+            _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+            for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+                _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+            }
+        }
+        this.lineChartData = _lineChartData;
+    }
+
+    // events
+    public chartClicked(e:any):void {
+        console.log(e);
+    }
+
+    public chartHovered(e:any):void {
+        console.log(e);
+    }
+    
+}
